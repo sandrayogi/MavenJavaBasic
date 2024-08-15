f8e615a21c496c3c6deeb58b904239a287426a37
Sandra Yogi Simangunsong <sandrayogi24@gmail.com>
2024-08-15 14:34:29 +0700

    Update java

--- src/main/java/com/automation/Main.java
+++ src/main/java/com/automation/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
+        //test hello world
         for (int i = 2; i <= 10; i++){
             System.out.println("i = " + i);
         }
