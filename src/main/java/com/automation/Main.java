78eee10ad660928edc13a8b9d986bf4c992f1ae5
Sandra Yogi Simangunsong <sandrayogi24@gmail.com>
2024-08-15 14:23:41 +0700

    New Java File

---
+++ src/main/java/com/automation/Main.java
@@ -0,0 +1,11 @@
+package com.automation;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        for (int i = 2; i <= 10; i++){
+            System.out.println("i = " + i);
+        }
+    }
+}
